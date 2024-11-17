f31fd146b8670e00189ec611790291471dd2c785
Saumay Gupta <148718955+Saumay-Gupta@users.noreply.github.com>
2024-11-17 14:52:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const path = require("path");
 const { spawn } = require('child_process');
+
 const app = express();
-
 app.set("view engine", "ejs")
-
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-    res.render("index",{result: null , segment: 0});
+    res.render("index", { segment: '', key: '', entity: '', total: "" ,flash:"",select:''});
 })
 
-app.post('/register', function (req, res) {
-    var t = req.body.analyse;
-    var m = req.body.marks;
-    console.log(m);
+app.post('/analyse', function (req, res) {
+    var t = req.body.input;
     var text = "" + t;
-    if (text.length == 0) res.send("Enter some text");
-    const pythonProcess = spawn('python3', ['python.py', text, m]);
+    if (text.length == 0) res.render("index", { segment: '', key: '', entity: '', total: "" , flash:'Enter some text please',select:''});
+    var m = req.body.marks;
+    if(typeof(m)=="undefined") res.render("index", { segment: '', key: '', entity: '', total: text , flash:'',select:'Select One of the above'});
+    var s = m.toString();
+    console.log(text.length)
+    const pythonProcess = spawn('python3', ['python.py', text, s]);
 
     let output = '';
     pythonProcess.stdout.on('data', (data) => {
@@ -33,11 +35,20 @@ app.post('/register', function (req, res) {
             return res.status(500).json({ error: 'Internal server error in Python script' });
         }
         try {
-            res.render("index",{result : output, segment: 0})
+            const parsedOutput = JSON.parse(output);
+            var s = parsedOutput.segment || '';
+            var k = parsedOutput.key || '';
+            var e = parsedOutput.entity || '';
+            k = JSON.stringify(k);
+            const arr_e = [];
+            for (var key in e) {
+                arr_e.push([key, e[key]]);
+            }
+            res.render("index", { segment: s, key: k.slice(1, -1).trim(), entity: arr_e, total: text ,flash:'',select:''});
         } catch (err) {
             res.status(500).json({ error: 'Error parsing Python output' });
         }
     });
 })
 
-app.listen(4500)
+app.listen(5500)
