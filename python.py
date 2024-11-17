0c19eb189848014f0359b3c5394074a82080f177
Saumay Gupta <148718955+Saumay-Gupta@users.noreply.github.com>
2024-11-17 14:54:08 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,33 +1,49 @@
-# from transformers import pipeline
-# from keybert import KeyBERT
-# import spacy
+import spacy
+import nltk
+# nltk.download('stopwords')
+from nltk.corpus import stopwords
+# nltk.download('punkt_tab')
+from nltk.tokenize import word_tokenize
+# import string
 import json
 
-# Load models once globally
-# sentiment_model = pipeline("sentiment-analysis")
-# keybert_model = KeyBERT()
-# try:
-#     ner_model = spacy.load("en_core_web_sm")
-# except OSError:
-#     import os
-#     os.system("python -m spacy download en_core_web_sm")
-#     ner_model = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 
-def analyze_text(text,m):
-    # key_phrases = keybert_model.extract_keywords(text, top_n=5, keyphrase_ngram_range=(1, 2))
-    # doc = ner_model(text)
-    # entities = [{'text': ent.text, 'label': ent.label_} for ent in doc.ents]
-    if(m == "segment"):
-        # sentiment = sentiment_model(text)
-        return json.dumps(text+"segment")
-    if(m == "keyphrase"):
-        return json.dumps(text+"keyphrase")
-    if(m == "entity"):
-        return json.dumps(text+"entity")
-    return json.dumps(text)
+def analyze_text(text,s):
+    result1 = ""
+    result2 = ""
+    result3 = ""
+    if("segment" in s):
+        result1 = "Positiive"
+    if("keyphrase" in s):
+        # Preprocessing the text
+        stop_words = set(stopwords.words("english"))
+        tokens = word_tokenize(text)
+    
+        # Remove punctuation and stop words
+        tokens = [word.lower() for word in tokens if word.isalpha() and word.lower() not in stop_words]
+    
+        # Use spaCy for noun phrase extraction
+        doc = nlp(" ".join(tokens))
+        key_phrases = [chunk.text for chunk in doc.noun_chunks]
+    
+        result2 = key_phrases
+    if("entity" in s):
+        # Process the text using the NLP model
+        doc = nlp(text)
+        # Extract entities and their labels
+        entities = [(ent.text, ent.label_) for ent in doc.ents]
+        # return entities
+        result3 = entities
+    output = {
+        "segment": result1,
+        "key": result2,
+        "entity": result3
+    }
+    return json.dumps(output)
 
 if __name__ == "__main__":
     import sys
     input_text = sys.argv[1]
-    input_value = sys.argv[2]
-    print(analyze_text(input_text,input_value))
+    input_text2 = sys.argv[2]
+    print(analyze_text(input_text,input_text2))
