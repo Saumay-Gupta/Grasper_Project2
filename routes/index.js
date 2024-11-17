169a70ef73e2666655b0cc558fe8f80ce920e6aa
Saumay Gupta <148718955+Saumay-Gupta@users.noreply.github.com>
2024-11-17 14:54:55 +0530

    Delete routes directory

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-// const express = require("express");
-
-// const app = express();
-
-// app.set("view engine" , "ejs")
-// app.get('/register',function(req,res){
-//     res.render("register");
-// })
